01c4714e0c790378e5a307edec053c9ba28ce81f
landrykf <landrykf@gmail.com>
2021-01-22 12:13:35 +0100

    mise en place du filtrqge de donnée reçu

--- routes/usersController.js
+++ routes/usersController.js
@@ -1,6 +1,12 @@
 const bcrypt = require('bcrypt');
 const jwtUtils =require('../utils/jwt.utils')
 const models = require ('../models');
+const asyncLib = require('async');
+
+//
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+const PASSWORD_REGEX = /^[a-zA-Z]\w{3,14}$/
 
 //Routes
 module.exports = {
@@ -20,6 +26,17 @@ module.exports = {
 
         //TODO verify pseudo length, mail regex..
 
+        if (username.length >= 13 || username.length <= 3){
+            return res.status(400).json({'error':'pseudo invalide (votre pseudo doit contenir entre 5 et 12 carachtères)'})
+        }
+
+        if (!EMAIL_REGEX.test(email)){
+            return res.status(400).json({'error': 'email invalid'})   
+        }
+
+        if (!PASSWORD_REGEX.test(password)){
+            return res.status(400).json({'error': 'mot de passe unvalide(le premier caractère du mot de passe doit être une lettre, il doit comporter entre 4 et 15 caractère sans caractère spéciaux sauf "_") '})
+        }
 
         //On vérifie si l'utilisateur existe déja dans la bdd
         models.User.findOne({
