f967ba9051859a4d6e246f4c4cdb475fc1d481e4
landrykf <landrykf@gmail.com>
2021-01-29 12:53:41 +0100

    gestion page d'accueil (populaire)(recherche)

---
+++ frontend/src/components/jikan/Header.js
@@ -0,0 +1,12 @@
+
+
+function Header(){
+    return (
+        <header>
+            <h1>SCAN CHECK</h1>
+            <h1>le meilleur site de suivi de scan</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ frontend/src/components/jikan/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Sidebar({ topManga }) {
+    return (
+        
+            <aside>
+                <nav>
+                    <h3>Populaires</h3>
+                    {topManga.map(manga => (
+
+                    <a 
+                        href={manga.url} 
+                        target="_blank"
+                        key = {manga.mal_id}
+                        rel="noreferrer">
+                        
+                        {manga.title}
+                    </a>
+
+                    ))}
+
+                </nav>
+            </aside>
+        
+    )
+}
+
+export default Sidebar
