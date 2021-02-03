c69319433c58478ac9836f380b9bd3769d578639
landrykf <landrykf@gmail.com>
2021-02-03 07:16:13 +0100

    fix erreur cors headers

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const server = express();
 
 server.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS,HEAD');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
